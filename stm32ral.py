fc07b98e93c93ed208a8507db0f3cc3d71ecd9f4
Adam Greig <adam@adamgreig.com>
2020-03-02 22:18:46 +0000

    Mark RegisterBlocks as repr(C)
    See https://github.com/imxrt-rs/imxrt-rs/issues/13,
    thanks @mciantyre

--- stm32ral.py
+++ stm32ral.py
@@ -766,6 +766,7 @@ class PeripheralPrototype(Node):
             address += register.size // 8
         lines = "\n".join(lines)
         return f"""
+        #[repr(C)]
         pub struct RegisterBlock {{
             {lines}
         }}"""
